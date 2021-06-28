9623c448b3cbadc78bd3d0d158bfee85da8870f7
Your Name <you@example.com>
2021-06-28 09:37:14 +0200

    Aggiunta view detail bambino

--- bambini/urls.py
+++ bambini/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import BambinoDetail
 urlpatterns = [
-    
+    path("<int:pk>", BambinoDetail.as_view(), name="bambini_detail")
 ]
--- bambini/views.py
+++ bambini/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.detail import DetailView
+from .models import Bambino
 # Create your views here.
+
+class BambinoDetail(DetailView):
+    model = Bambino
+    template_name = 'bambino_detail.html'
