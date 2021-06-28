4f2696a0641c45391d4f36e0ec84c9a673084e4f
Your Name <you@example.com>
2021-06-28 19:17:40 +0200

    Aggiunta lista bambini

--- bambini/urls.py
+++ bambini/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import BambinoDetail
+from .views import BambinoDetail, BambinoList
 urlpatterns = [
-    path("<int:pk>", BambinoDetail.as_view(), name="bambini_detail")
+    path("<int:pk>", BambinoDetail.as_view(), name="bambini_detail"),
+    path("", BambinoList.as_view(), name="bambini_list")
 ]
--- bambini/views.py
+++ bambini/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Bambino
 # Create your views here.
+
 
 class BambinoDetail(DetailView):
     model = Bambino
     template_name = 'bambino_detail.html'
+
+
+class BambinoList(ListView):
+    model = Bambino
+    template_name = 'bambino_list.html'
