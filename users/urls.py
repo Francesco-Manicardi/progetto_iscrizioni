92a744d0b4001ec5ed0c4d32617e6b3f3be2ebd8
Your Name <you@example.com>
2021-07-27 10:35:13 +0200

    Small changes to navbar and user urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,reverse_lazy
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('create', views.UserCreateView.as_view(), name="register"),
-    path('user_edit', views.UserUpdateView.as_view(), name="user_edit"),
+    path('user_edit', login_required(views.UserUpdateView.as_view()), name="user_edit"),
     path('login', auth_views.LoginView.as_view(), name="login"),
     path('logout', auth_views.LogoutView.as_view(next_page=reverse_lazy("login")), name="logout"),
 ]
